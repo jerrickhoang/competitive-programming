582488b0c170a92017258f3e9aaa43a65b7f5655
Jerrick Hoang <jerrickhoang@gmail.com>
2018-07-01 18:00:20 -0500

    Solve UVA 11498

---
+++ uva/adhoc/easy/11498.cpp
@@ -0,0 +1,106 @@
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+//math
+//-------------------------------------------
+template<class T> inline T sqr(T x) {return x*x;}
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+//container util
+//------------------------------------------
+#define ALL(a)  (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+// BEGIN CUT HERE
+int main() {
+  int k;
+  k = -1;
+  while (k != 0)
+  {
+    cin >> k;
+    int n, m;
+    cin >> n >> m;
+    REP(i, k)
+    {
+      int a, b;
+      cin >> a >> b;
+      if (a == n || b == m)
+      {
+        cout << "divisa" << endl;
+      }
+      else if (a > n && b > m)
+      {
+        cout << "NE" << endl;
+      }
+      else if (a > n && b < m)
+      {
+        cout << "SE" << endl;
+      }
+      else if (a < n && b < m)
+      {
+        cout << "SO" << endl;
+      }
+      else if (a < n && b > m)
+      {
+        cout << "NO" << endl;
+      }
+    }
+  }
+}
